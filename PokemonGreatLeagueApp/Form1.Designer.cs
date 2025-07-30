fdc25a7cdae7c917d675491f60c307e13fba9856
Ben Leonard <ben@clubready.com>
2025-07-30 17:35:47 -0500

    Update to add shields

--- PokemonGreatLeagueApp/Form1.Designer.cs
+++ PokemonGreatLeagueApp/Form1.Designer.cs
@@ -14,6 +14,10 @@
         private System.Windows.Forms.Button resetMyTeamButton;
         private System.Windows.Forms.Button resetEnemyTeamButton;
         private System.Windows.Forms.Button resetAllButton;
+        private System.Windows.Forms.CheckBox myShield1;
+        private System.Windows.Forms.CheckBox myShield2;
+        private System.Windows.Forms.CheckBox enemyShield1;
+        private System.Windows.Forms.CheckBox enemyShield2;
 
         private void InitializeComponent()
         {
@@ -28,6 +32,10 @@
             this.resetMyTeamButton = new System.Windows.Forms.Button();
             this.resetEnemyTeamButton = new System.Windows.Forms.Button();
             this.resetAllButton = new System.Windows.Forms.Button();
+            this.myShield1 = new System.Windows.Forms.CheckBox();
+            this.myShield2 = new System.Windows.Forms.CheckBox();
+            this.enemyShield1 = new System.Windows.Forms.CheckBox();
+            this.enemyShield2 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
 
             // leagueDropdown
@@ -65,27 +73,47 @@
             this.enemyTeam3.Location = new System.Drawing.Point(200, 140);
             this.enemyTeam3.Size = new System.Drawing.Size(150, 23);
 
+            // myShield1
+            this.myShield1.Text = "My Shield 1";
+            this.myShield1.Location = new System.Drawing.Point(20, 180);
+            this.myShield1.AutoSize = true;
+
+            // myShield2
+            this.myShield2.Text = "My Shield 2";
+            this.myShield2.Location = new System.Drawing.Point(20, 210);
+            this.myShield2.AutoSize = true;
+
+            // enemyShield1
+            this.enemyShield1.Text = "Enemy Shield 1";
+            this.enemyShield1.Location = new System.Drawing.Point(200, 180);
+            this.enemyShield1.AutoSize = true;
+
+            // enemyShield2
+            this.enemyShield2.Text = "Enemy Shield 2";
+            this.enemyShield2.Location = new System.Drawing.Point(200, 210);
+            this.enemyShield2.AutoSize = true;
+
             // coverageBox
-            this.coverageBox.Location = new System.Drawing.Point(20, 180);
+            this.coverageBox.Location = new System.Drawing.Point(20, 250);
             this.coverageBox.Size = new System.Drawing.Size(330, 120);
 
             // resetMyTeamButton
             this.resetMyTeamButton.Text = "Reset My Team";
-            this.resetMyTeamButton.Location = new System.Drawing.Point(20, 320);
+            this.resetMyTeamButton.Location = new System.Drawing.Point(20, 390);
             this.resetMyTeamButton.Size = new System.Drawing.Size(100, 30);
 
             // resetEnemyTeamButton
             this.resetEnemyTeamButton.Text = "Reset Enemy";
-            this.resetEnemyTeamButton.Location = new System.Drawing.Point(130, 320);
+            this.resetEnemyTeamButton.Location = new System.Drawing.Point(130, 390);
             this.resetEnemyTeamButton.Size = new System.Drawing.Size(100, 30);
 
             // resetAllButton
             this.resetAllButton.Text = "Reset All";
-            this.resetAllButton.Location = new System.Drawing.Point(240, 320);
+            this.resetAllButton.Location = new System.Drawing.Point(240, 390);
             this.resetAllButton.Size = new System.Drawing.Size(100, 30);
 
             // Form1
-            this.ClientSize = new System.Drawing.Size(380, 370);
+            this.ClientSize = new System.Drawing.Size(380, 440);
             this.Controls.Add(this.leagueDropdown);
             this.Controls.Add(this.myTeam1);
             this.Controls.Add(this.myTeam2);
@@ -93,6 +121,10 @@
             this.Controls.Add(this.enemyTeam1);
             this.Controls.Add(this.enemyTeam2);
             this.Controls.Add(this.enemyTeam3);
+            this.Controls.Add(this.myShield1);
+            this.Controls.Add(this.myShield2);
+            this.Controls.Add(this.enemyShield1);
+            this.Controls.Add(this.enemyShield2);
             this.Controls.Add(this.coverageBox);
             this.Controls.Add(this.resetMyTeamButton);
             this.Controls.Add(this.resetEnemyTeamButton);
@@ -100,6 +132,7 @@
             this.Name = "Form1";
             this.Text = "Nebsoji PvP Trainer";
             this.ResumeLayout(false);
+            this.PerformLayout();
         }
     }
 }
